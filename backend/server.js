becc14a79013f1f438411ab4437df57ffe1839d7
Osas706 <92319443+Osas706@users.noreply.github.com>
2024-04-21 21:40:02 +0100

    sign up controler

--- backend/server.js
+++ backend/server.js
@@ -2,20 +2,25 @@ import express  from "express";
 import dotenv  from "dotenv";
 
 import authRoutes from "./routes/auth.routes.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 dotenv.config();
 
+app.use(express.json());
 
-const PORT = process.env.PORT || 5000;
+// to parse the incoming requests with JSON payload (from req.body)
+app.use("/api/auth", authRoutes);
+ 
 
-app.get("/", (req, res) => {
+//app.get("/", (req, res) => {
     //root route http://localhost:5000
+   // res.send("Hello");
+//})
 
-    res.send("Hello");
-})
-
-app.use("/api/auth", authRoutes)
-
-app.listen(PORT, () => console.log(`Server running on port ${PORT}` ));
+app.listen(PORT, () => {
+    connectToMongoDB();
+   console.log(`Server running on port ${PORT}`);
+});
