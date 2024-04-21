e990b81e115e579bea9517fd5c34b0acaa1ccf42
Osas706 <92319443+Osas706@users.noreply.github.com>
2024-04-21 10:52:49 +0100

    setup

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,16 @@
+export const signup = async(req, res) => {
+    try {
+       
+    } catch (error) {
+        
+    }
+};
+
+export const login = (req, res) => {
+    console.log("Login");
+};
+
+export const logout = (req, res) => {
+    console.log("Logout");
+};
+
