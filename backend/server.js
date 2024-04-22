fdb6aa85ff90b1aeb8f5ddb8f8768166b85ef406
Osas706 <92319443+Osas706@users.noreply.github.com>
2024-04-22 10:46:16 +0100

    getmessage nd sendmessage controlers

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import express  from "express";
 import dotenv  from "dotenv";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
@@ -9,10 +11,14 @@ const PORT = process.env.PORT || 5000;
 
 dotenv.config();
 
+// to parse the incoming requests with JSON payload (from req.body)
 app.use(express.json());
 
-// to parse the incoming requests with JSON payload (from req.body)
+app.use(cookieParser());
+
+
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
  
 
 //app.get("/", (req, res) => {
